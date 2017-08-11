d303034aa746e6bf04f8b22aa5e5bd055d8cb5c5
quixote15 <santos2tiago@gmail.com>
2017-08-11 20:48:30 -0300

    SpellChecker search and user input

--- src/editor/de/texto/SpellChecker.java
+++ src/editor/de/texto/SpellChecker.java
@@ -8,6 +8,8 @@ package editor.de.texto;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *  This class processes the documents and consults the user about unknown words
@@ -16,13 +18,20 @@ import java.io.IOException;
  */
 public class SpellChecker {
     
+   private ArrayList<String> ignored;
    
+   SpellChecker(){
+       ignored = new ArrayList<String>();
+   }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
        // Dictionary dic = new Dictionary();
+        SpellChecker speller = new SpellChecker();
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        
         WordSet set = new WordSet();
         Word w = new Word();
         
@@ -33,11 +42,48 @@ public class SpellChecker {
         //dic.save("brakeq brakequw",dic.getFilepath());
 	
         for(String s : w.getInputWords()){
+            if(speller.isValidWord(set, s)){
+                //add to file
+            }
+            else{
+                //check if the user wants to add this word to dictionary
+                System.out.println("The word " + s + " is unkown on dictionary. "
+                        + "Do you wish to add this word to the dictionary? Yes = 1, No = 0\n");
+                int n = reader.nextInt(); // Scans the next token of the input as an int.
+                if(n==1){
+                    //add to dictionary
+                }
+                else{
+                    //add this word to the ignored set of words
+                }
+                
+            }
             System.out.println(s);
         }
 
         
         
     }
+    
+    /**
+     * 
+     * 
+     * 
+     * @param set
+     * @param word
+     * @return 
+     */
+    
+    boolean isValidWord(WordSet set,String word){
+        for(String current: set.getWords()){
+            if(current.matches(word))
+                   return true; 
+        }
+        
+        return false;
+    }
+    
+    
+    
     
 }
